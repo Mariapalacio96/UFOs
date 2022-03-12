812d0d9b04d30b3d5a595d8ed5a00455918ad1c4
maria palacio <palamari@kean.edu>
2022-03-12 07:35:06 -0500

    filtering dates

--- static/js/app.js
+++ static/js/app.js
@@ -5,7 +5,7 @@ const tableData = data;
 var tbody = d3.select("tbody");
 
 // Build the table
-function builTable(data) {
+function buildTable(data) {
     // First, clear out any existing data
     tbody.html("");
 
@@ -24,3 +24,26 @@ function builTable(data) {
         );
     });
 }
+
+function handleClick(){
+    // Grab the datetime value from the filter
+    let data = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // Check to see if a date was entered and filter the data using that date
+    if (date){
+        // Apply filter to the table data to only keeo the rows where the datetime value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+
+    // Rebuiild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will just be the original tableData
+    buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
+
